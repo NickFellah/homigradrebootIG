113304bff2b1f8388d43c46314b3a659bb7aeb20
WhangaTy <WhangaTy@gmail.com>
2025-09-16 19:02:56 +1200

    fixes and stuff

--- gamemodes/homigrad/gamemode/game/levels/hideandseek/init_sv.lua
+++ gamemodes/homigrad/gamemode/game/levels/hideandseek/init_sv.lua
@@ -119,6 +119,11 @@ end
 function hideandseek.EndRound(winner) tdm.EndRoundMessage(winner) end
 
 function hideandseek.PlayerSpawn(ply,teamID)
+	 if not teamID or not hideandseek.teamEncoder[teamID] then
+        print("[Hide & Seek] Invalid teamID for", ply, "teamID:", teamID)
+        return
+    end
+
 	local teamTbl = hideandseek[hideandseek.teamEncoder[teamID]]
 	local color = teamTbl[2]
     
